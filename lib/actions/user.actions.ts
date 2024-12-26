87d61d7b8e6731159375fd328ffe48d038581318
MichaelC1995 <m.chatellier@yahoo.com>
2024-12-26 14:20:24 -0600

    ignore build error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -100,12 +100,12 @@ export const getCurrentUser = async () => {
     const result = await account.get();
 
     const user = await databases.listDocuments(
-      appwriteConfig.databaseId,
-      appwriteConfig.usersCollectionId,
-      [Query.equal("accountId", result.$id)],
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("accountId", result.$id)],
     );
 
-    if (user.total < 1) return null;
+    if (user.total <= 0) return null;
 
     return parseStringify(user.documents[0]);
   } catch (error) {
--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -6,12 +6,12 @@ import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
   const client = new Client()
-    .setEndpoint(appwriteConfig.endpointUrl)
-    .setProject(appwriteConfig.projectId);
+      .setEndpoint(appwriteConfig.endpointUrl)
+      .setProject(appwriteConfig.projectId);
 
   const session = (await cookies()).get("appwrite-session");
 
-  // if (!session || !session.value) throw new Error("No session");
+  if (!session || !session.value) throw new Error("No session");
 
   client.setSession(session.value);
 
@@ -27,9 +27,9 @@ export const createSessionClient = async () => {
 
 export const createAdminClient = async () => {
   const client = new Client()
-    .setEndpoint(appwriteConfig.endpointUrl)
-    .setProject(appwriteConfig.projectId)
-    .setKey(appwriteConfig.secretKey);
+      .setEndpoint(appwriteConfig.endpointUrl)
+      .setProject(appwriteConfig.projectId)
+      .setKey(appwriteConfig.secretKey);
 
   return {
     get account() {
